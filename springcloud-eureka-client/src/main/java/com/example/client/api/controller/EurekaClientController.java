c4d5fb71b9a64c9390d7f8d1ec802efa10826ce5
EricChang82 <changlejc@Hotmail.com>
2019-04-17 15:28:26 +0800

    1

--- springcloud-eureka-client/src/main/java/com/example/client/api/controller/EurekaClientController.java
+++ springcloud-eureka-client/src/main/java/com/example/client/api/controller/EurekaClientController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EurekaClientController {
      
-    @Value("${server.port}")
+    @Value("${server.port}") 
     private String port;
  
     /**
